d6caa69f943e9fa19f007cb2d523bfe7fbe064f2
Avery <averypeckstl@gmail.com>
2020-12-02 00:59:45 -0600

    day 2 solutions

---
+++ day2.c
@@ -0,0 +1,50 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct policy {
+	int low;
+	int high;
+	char letter;
+} policy;
+
+int checkPassword(policy *pol, char *password) {
+	int len = strlen(password);
+	int occurrences = 0;
+
+	for (int i = 0; i < len; i++) {
+		if (password[i] == pol->letter) {
+			occurrences++;
+		}
+	}
+	
+	if (occurrences >= pol->low && occurrences <= pol->high)
+		return 1;
+
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	FILE *fp;
+
+	if (!(fp = fopen(argv[1],"r"))) {
+		return 255;
+	}
+	int numPasswords = 1000;
+
+	policy *pol = calloc(1,sizeof(policy));
+	char *password = calloc(1,30);
+
+	int validPasswords = 0;
+
+	// read all passwords and policies from file
+	for (int i = 0; i < numPasswords; i++) {
+		fscanf(fp, "%d-%d %c: %s", &(pol->low), &(pol->high), &(pol->letter), password);
+		if (checkPassword(pol, password)) 
+			validPasswords++;
+	}
+	
+	printf("Valid passwords = %d\n", validPasswords);
+
+	return 0;
+}
